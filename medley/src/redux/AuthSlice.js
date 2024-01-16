3e7e85bf07129f41979322e1a40f3fed6c6b1bad
Aditya Pathak <sumitpathak861@gmail.com>
2024-01-16 11:31:57 +0530

    localStorage Setup.✅

--- medley/src/redux/AuthSlice.js
+++ medley/src/redux/AuthSlice.js
@@ -1,14 +1,23 @@
 import {createSlice} from "@reduxjs/toolkit"
 
+const localValues = JSON.parse(localStorage.getItem("userInfo"))
+console.log(localValues);
+
+const initialState = {
+    value : localValues || {
+        token:undefined , name : undefined , type : undefined , isLogin : false
+    }
+}
+console.log(initialState);
+
 const slice = createSlice({
     name: "userDetails",
-    initialState:{
-        value:{token:undefined , name:undefined , type:undefined , isLogin:false}
-    },
+    initialState,
     reducers:{
         authReducer:(state,action) => {
             state.value= action.payload;
             console.log(action.payload);
+            localStorage.setItem('userInfo' , JSON.stringify(action.payload))
         }
     }
 })
